14663a257599bcac02c3238981011b9e4b0b7f39
dndsza1 <67662140+dndsza1@users.noreply.github.com>
2020-07-03 23:35:57 -0300

    Create api.php

---
+++ api.php
@@ -0,0 +1,29 @@
+<?php  
+
+error_reporting(0);
+
+if (isset($_GET['bin'])) {
+	$bin=substr($_GET['bin'], 0,6);
+
+	$ch=curl_init();
+	curl_setopt($ch, CURLOPT_URL, 'https://bincheck.org/'.$bin);
+	curl_setopt($ch, CURLOPT_HEADER, 1);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+	$e=curl_exec($ch);
+	$split=explode('</td>',$e);
+	$split2=explode('<td>',$e);
+
+
+	function rep($str){
+	return trim(strip_tags(preg_replace('/<(head|title|style|script)[^>]*>.*>\/\\1>/s', '', $str))); 
+	}
+	 echo str_replace('Brand', '', 'Cartão: '.rep($split[1])).'<br>';
+     echo str_replace('Type', '', 'Tipo: '.rep($split[2])).'<br>';
+     echo str_replace('Category', '', 'Level: '.rep($split[3])).'<br>';
+     echo str_replace('Bank URL', '', 'Banco: '.rep($split2[5])).'<br>';
+     echo str_replace('Country', '', 'País: '.rep($split[6])).'<br>';
+}
+    ?>
